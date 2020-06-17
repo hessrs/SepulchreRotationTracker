20fbd2b11d77ab867187ca098eee6eb7d010fdc9
hessrs <follmerka+1@gmail.com>
2020-06-16 23:48:01 -0600

    Add project files.

---
+++ SepulchreTracker/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace SepulchreTracker
+{
+    partial class sepulchredRotationCounter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(sepulchredRotationCounter));
+			this.westEntranceN = new System.Windows.Forms.Button();
+			this.westEntranceLabel = new System.Windows.Forms.Label();
+			this.westEntranceW = new System.Windows.Forms.Button();
+			this.westEntranceE = new System.Windows.Forms.Button();
+			this.westEntranceS = new System.Windows.Forms.Button();
+			this.eastEntranceS = new System.Windows.Forms.Button();
+			this.eastEntranceE = new System.Windows.Forms.Button();
+			this.eastEntranceW = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.eastEntranceN = new System.Windows.Forms.Button();
+			this.buttonReset = new System.Windows.Forms.Button();
+			this.labelFloor = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// westEntranceN
+			// 
+			this.westEntranceN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.westEntranceN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.westEntranceN.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.westEntranceN.ForeColor = System.Drawing.Color.White;
+			this.westEntranceN.Location = new System.Drawing.Point(51, 36);
+			this.westEntranceN.Name = "westEntranceN";
+			this.westEntranceN.Size = new System.Drawing.Size(84, 58);
+			this.westEntranceN.TabIndex = 0;
+			this.westEntranceN.Text = "North";
+			this.westEntranceN.UseVisualStyleBackColor = false;
+			// 
+			// westEntranceLabel
+			// 
+			this.westEntranceLabel.AutoSize = true;
+			this.westEntranceLabel.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.westEntranceLabel.Location = new System.Drawing.Point(29, 9);
+			this.westEntranceLabel.Name = "westEntranceLabel";
+			this.westEntranceLabel.Size = new System.Drawing.Size(135, 22);
+			this.westEntranceLabel.TabIndex = 1;
+			this.westEntranceLabel.Text = "West Entrance";
+			// 
+			// westEntranceW
+			// 
+			this.westEntranceW.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.westEntranceW.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.westEntranceW.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.westEntranceW.ForeColor = System.Drawing.Color.White;
+			this.westEntranceW.Location = new System.Drawing.Point(3, 100);
+			this.westEntranceW.Name = "westEntranceW";
+			this.westEntranceW.Size = new System.Drawing.Size(84, 58);
+			this.westEntranceW.TabIndex = 2;
+			this.westEntranceW.Text = "West";
+			this.westEntranceW.UseVisualStyleBackColor = false;
+			// 
+			// westEntranceE
+			// 
+			this.westEntranceE.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.westEntranceE.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.westEntranceE.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.westEntranceE.ForeColor = System.Drawing.Color.White;
+			this.westEntranceE.Location = new System.Drawing.Point(102, 100);
+			this.westEntranceE.Name = "westEntranceE";
+			this.westEntranceE.Size = new System.Drawing.Size(84, 58);
+			this.westEntranceE.TabIndex = 3;
+			this.westEntranceE.Text = "East";
+			this.westEntranceE.UseVisualStyleBackColor = false;
+			// 
+			// westEntranceS
+			// 
+			this.westEntranceS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.westEntranceS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.westEntranceS.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.westEntranceS.ForeColor = System.Drawing.Color.White;
+			this.westEntranceS.Location = new System.Drawing.Point(51, 164);
+			this.westEntranceS.Name = "westEntranceS";
+			this.westEntranceS.Size = new System.Drawing.Size(84, 58);
+			this.westEntranceS.TabIndex = 4;
+			this.westEntranceS.Text = "South";
+			this.westEntranceS.UseVisualStyleBackColor = false;
+			// 
+			// eastEntranceS
+			// 
+			this.eastEntranceS.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.eastEntranceS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.eastEntranceS.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.eastEntranceS.ForeColor = System.Drawing.Color.White;
+			this.eastEntranceS.Location = new System.Drawing.Point(255, 164);
+			this.eastEntranceS.Name = "eastEntranceS";
+			this.eastEntranceS.Size = new System.Drawing.Size(84, 58);
+			this.eastEntranceS.TabIndex = 9;
+			this.eastEntranceS.Text = "South";
+			this.eastEntranceS.UseVisualStyleBackColor = false;
+			// 
+			// eastEntranceE
+			// 
+			this.eastEntranceE.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.eastEntranceE.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.eastEntranceE.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.eastEntranceE.ForeColor = System.Drawing.Color.White;
+			this.eastEntranceE.Location = new System.Drawing.Point(298, 100);
+			this.eastEntranceE.Name = "eastEntranceE";
+			this.eastEntranceE.Size = new System.Drawing.Size(84, 58);
+			this.eastEntranceE.TabIndex = 8;
+			this.eastEntranceE.Text = "East";
+			this.eastEntranceE.UseVisualStyleBackColor = false;
+			// 
+			// eastEntranceW
+			// 
+			this.eastEntranceW.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.eastEntranceW.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.eastEntranceW.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.eastEntranceW.ForeColor = System.Drawing.Color.White;
+			this.eastEntranceW.Location = new System.Drawing.Point(207, 100);
+			this.eastEntranceW.Name = "eastEntranceW";
+			this.eastEntranceW.Size = new System.Drawing.Size(84, 58);
+			this.eastEntranceW.TabIndex = 7;
+			this.eastEntranceW.Text = "West";
+			this.eastEntranceW.UseVisualStyleBackColor = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(234, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(128, 22);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "East Entrance";
+			// 
+			// eastEntranceN
+			// 
+			this.eastEntranceN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.eastEntranceN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.eastEntranceN.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.eastEntranceN.ForeColor = System.Drawing.Color.White;
+			this.eastEntranceN.Location = new System.Drawing.Point(255, 36);
+			this.eastEntranceN.Name = "eastEntranceN";
+			this.eastEntranceN.Size = new System.Drawing.Size(84, 58);
+			this.eastEntranceN.TabIndex = 5;
+			this.eastEntranceN.Text = "North";
+			this.eastEntranceN.UseVisualStyleBackColor = false;
+			// 
+			// buttonReset
+			// 
+			this.buttonReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+			this.buttonReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonReset.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonReset.ForeColor = System.Drawing.Color.White;
+			this.buttonReset.Location = new System.Drawing.Point(162, 36);
+			this.buttonReset.Name = "buttonReset";
+			this.buttonReset.Size = new System.Drawing.Size(66, 25);
+			this.buttonReset.TabIndex = 10;
+			this.buttonReset.Text = "Reset";
+			this.buttonReset.UseVisualStyleBackColor = false;
+			this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+			// 
+			// labelFloor
+			// 
+			this.labelFloor.AutoSize = true;
+			this.labelFloor.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.labelFloor.Location = new System.Drawing.Point(174, 64);
+			this.labelFloor.Name = "labelFloor";
+			this.labelFloor.Size = new System.Drawing.Size(42, 13);
+			this.labelFloor.TabIndex = 11;
+			this.labelFloor.Text = "Floor: 1";
+			this.labelFloor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// sepulchredRotationCounter
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.DimGray;
+			this.ClientSize = new System.Drawing.Size(394, 232);
+			this.Controls.Add(this.labelFloor);
+			this.Controls.Add(this.buttonReset);
+			this.Controls.Add(this.eastEntranceS);
+			this.Controls.Add(this.eastEntranceE);
+			this.Controls.Add(this.eastEntranceW);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.eastEntranceN);
+			this.Controls.Add(this.westEntranceS);
+			this.Controls.Add(this.westEntranceE);
+			this.Controls.Add(this.westEntranceW);
+			this.Controls.Add(this.westEntranceLabel);
+			this.Controls.Add(this.westEntranceN);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "sepulchredRotationCounter";
+			this.Text = "Sepulchre Rotation Counter";
+			this.Load += new System.EventHandler(this.sepulchreRotationCounter_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button westEntranceN;
+        private System.Windows.Forms.Label westEntranceLabel;
+        private System.Windows.Forms.Button westEntranceW;
+        private System.Windows.Forms.Button westEntranceE;
+        private System.Windows.Forms.Button westEntranceS;
+        private System.Windows.Forms.Button eastEntranceS;
+        private System.Windows.Forms.Button eastEntranceE;
+        private System.Windows.Forms.Button eastEntranceW;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button eastEntranceN;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Label labelFloor;
+    }
+}
+
---
+++ SepulchreTracker/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SepulchreTracker {
+    public partial class sepulchredRotationCounter : Form {
+
+        String rotationString = "";
+        int currentFloor = 1;
+        bool onReset = false;
+
+        readonly int numberOfFloorsWithMultiplePaths = 4;
+        readonly String ENTRANCE_DIRECTION_WEST = "w";
+        readonly String ENTRANCE_DIRECTION_EAST = "e";
+
+        public sepulchredRotationCounter() {
+            InitializeComponent();
+        }
+
+        private void sepulchreRotationCounter_Load(object sender, EventArgs e) {
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+
+            westEntranceN.Click += buttonClick;
+            westEntranceE.Click += buttonClick;
+            westEntranceS.Click += buttonClick;
+            westEntranceW.Click += buttonClick;
+            eastEntranceN.Click += buttonClick;
+            eastEntranceE.Click += buttonClick;
+            eastEntranceS.Click += buttonClick;
+            eastEntranceW.Click += buttonClick;
+
+            westEntranceN.FlatAppearance.BorderColor = Color.Black;
+            westEntranceN.FlatAppearance.BorderSize = 1;
+            westEntranceE.FlatAppearance.BorderColor = Color.Black;
+            westEntranceE.FlatAppearance.BorderSize = 1;
+            westEntranceS.FlatAppearance.BorderColor = Color.Black;
+            westEntranceS.FlatAppearance.BorderSize = 1;
+            westEntranceW.FlatAppearance.BorderColor = Color.Black;
+            westEntranceW.FlatAppearance.BorderSize = 1;
+            eastEntranceN.FlatAppearance.BorderColor = Color.Black;
+            eastEntranceN.FlatAppearance.BorderSize = 1;
+            eastEntranceE.FlatAppearance.BorderColor = Color.Black;
+            eastEntranceE.FlatAppearance.BorderSize = 1;
+            eastEntranceS.FlatAppearance.BorderColor = Color.Black;
+            eastEntranceS.FlatAppearance.BorderSize = 1;
+            eastEntranceW.FlatAppearance.BorderColor = Color.Black;
+            eastEntranceW.FlatAppearance.BorderSize = 1;
+
+        }
+        private void buttonReset_Click(object sender, EventArgs e) {
+
+            writeRotationToFile();
+            currentFloor = 1;
+            rotationString = "";
+            labelFloor.Text = "Floor: " + currentFloor;
+
+            enableAppropriateDirectionsForFloor(currentFloor, "RESET");
+        }
+
+        private void buttonClick(object src, EventArgs args) {
+
+            Button clickedButton = (src as Button);
+            String clickedButtonName = clickedButton.Name.ToLower();
+            String direction = clickedButton.Name.Substring(clickedButton.Name.Length - 1, 1);
+            String entranceLobbyDirection = clickedButtonName.Substring(0, 1);
+
+            if (currentFloor == 1) {
+                rotationString += entranceLobbyDirection; // inserts whether we took the west or east entrance
+            }
+
+            // + 1 to account for the lobby direction
+            if (rotationString.Length < (numberOfFloorsWithMultiplePaths + 1)) {
+                rotationString += direction;
+            }
+            else {
+                Console.WriteLine("Could not add direction since you already finished floor " + (numberOfFloorsWithMultiplePaths) + ".");
+			}
+            
+            if (currentFloor >= numberOfFloorsWithMultiplePaths) {
+                currentFloor = numberOfFloorsWithMultiplePaths;
+            }
+
+            if ((currentFloor + 1) < 5) {
+                currentFloor++;
+            }
+            
+            enableAppropriateDirectionsForFloor(currentFloor, entranceLobbyDirection);
+
+            Console.WriteLine("Current Rotation String: " + rotationString);
+            Console.WriteLine("Current floor: " + currentFloor);
+            labelFloor.Text = "Floor: " + currentFloor;
+        }
+
+        private void writeRotationToFile() {
+
+            // + 1 to account for the lobby direction
+            if (rotationString.Length != (numberOfFloorsWithMultiplePaths + 1)) { 
+                Console.WriteLine("rotationString \"" + rotationString + "\" is not 4 characters long.");
+                return;
+			}
+
+            try {
+                StreamWriter file = new System.IO.StreamWriter(System.IO.Directory.GetCurrentDirectory() + "/sepulchreRotations.txt", true);
+                file.WriteLine(rotationString);
+                file.Close();
+            }
+            catch (IOException e) {
+                MessageBox.Show(e.ToString());
+			}
+        }
+
+        private void enableAppropriateDirectionsForFloor(int floor, String lobbyEntranceDirection) {
+            // test 23
+
+            if (lobbyEntranceDirection.Equals("RESET")) { 
+                westEntranceN.Enabled = true;
+                westEntranceE.Enabled = true;
+                westEntranceS.Enabled = true;
+                westEntranceW.Enabled = true;
+
+                eastEntranceN.Enabled = true;
+                eastEntranceE.Enabled = true;
+                eastEntranceS.Enabled = true;
+                eastEntranceW.Enabled = true;
+
+                return;
+            }
+
+            if (floor == 1) {
+                if (lobbyEntranceDirection.Equals(ENTRANCE_DIRECTION_WEST)) {
+                    westEntranceN.Enabled = true;
+                    westEntranceE.Enabled = true;
+                    westEntranceS.Enabled = true;
+                    westEntranceW.Enabled = true;
+                    eastEntranceN.Enabled = false;
+                    eastEntranceE.Enabled = false;
+                    eastEntranceS.Enabled = false;
+                    eastEntranceW.Enabled = false;
+                }
+                else {
+                    westEntranceN.Enabled = false;
+                    westEntranceE.Enabled = false;
+                    westEntranceS.Enabled = false;
+                    westEntranceW.Enabled = false;
+                    eastEntranceN.Enabled = true;
+                    eastEntranceE.Enabled = true;
+                    eastEntranceS.Enabled = true;
+                    eastEntranceW.Enabled = true;
+                }
+            }
+            else if (floor == 2) {
+                if (lobbyEntranceDirection.Equals(ENTRANCE_DIRECTION_WEST)) {
+                    westEntranceN.Enabled = true;
+                    westEntranceE.Enabled = true;
+                    westEntranceS.Enabled = true;
+                    westEntranceW.Enabled = true;
+
+                    eastEntranceN.Enabled = false;
+                    eastEntranceE.Enabled = false;
+                    eastEntranceS.Enabled = false;
+                    eastEntranceW.Enabled = false;
+                }
+                else {
+                    westEntranceN.Enabled = false;
+                    westEntranceE.Enabled = false;
+                    westEntranceS.Enabled = false;
+                    westEntranceW.Enabled = false;
+
+                    eastEntranceN.Enabled = true;
+                    eastEntranceE.Enabled = true;
+                    eastEntranceS.Enabled = true;
+                    eastEntranceW.Enabled = true;
+                }
+            }
+            else if (floor == 3) {
+                if (lobbyEntranceDirection.Equals(ENTRANCE_DIRECTION_WEST)) {
+                    westEntranceN.Enabled = false;
+                    westEntranceE.Enabled = true;
+                    westEntranceS.Enabled = false;
+                    westEntranceW.Enabled = true;
+
+                    eastEntranceN.Enabled = false;
+                    eastEntranceE.Enabled = false;
+                    eastEntranceS.Enabled = false;
+                    eastEntranceW.Enabled = false;
+                }
+                else {
+                    westEntranceN.Enabled = false;
+                    westEntranceE.Enabled = false;
+                    westEntranceS.Enabled = false;
+                    westEntranceW.Enabled = false;
+
+                    eastEntranceN.Enabled = false;
+                    eastEntranceE.Enabled = true;
+                    eastEntranceS.Enabled = false;
+                    eastEntranceW.Enabled = true;
+                }
+            }
+            else if (floor == 4) {
+                if (lobbyEntranceDirection.Equals(ENTRANCE_DIRECTION_WEST)) {
+                    westEntranceN.Enabled = true;
+                    westEntranceE.Enabled = false;
+                    westEntranceS.Enabled = true;
+                    westEntranceW.Enabled = false;
+
+                    eastEntranceN.Enabled = false;
+                    eastEntranceE.Enabled = false;
+                    eastEntranceS.Enabled = false;
+                    eastEntranceW.Enabled = false;
+                }
+                else {
+                    westEntranceN.Enabled = false;
+                    westEntranceE.Enabled = false;
+                    westEntranceS.Enabled = false;
+                    westEntranceW.Enabled = false;
+
+                    eastEntranceN.Enabled = true;
+                    eastEntranceE.Enabled = false;
+                    eastEntranceS.Enabled = true;
+                    eastEntranceW.Enabled = false;
+                }
+            }
+        }
+    }
+}
